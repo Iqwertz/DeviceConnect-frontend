c2515bc8826a2134ce1073ad800e06d541b513b5
Julius <juliushussl@gmail.com>
2020-08-17 15:45:47 +0200

    join session component

--- src/app/components/join-session/join-session.component.ts
+++ src/app/components/join-session/join-session.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-join-session',
   templateUrl: './join-session.component.html',
-  styleUrls: ['./join-session.component.scss']
+  styleUrls: ['./join-session.component.scss'],
 })
 export class JoinSessionComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  joinSession(id: string) {
+    id = id.toUpperCase();
+    this.router.navigate(['session', id]);
   }
 
+  inputKey(event) {
+    if (event.key === 'Backspace') {
+      let element = event.srcElement.previousElementSibling; // get the sibling element
+
+      if (element == null) {
+        // check if its null
+        return;
+      } else {
+        element.focus(); // focus if not null
+      }
+    } else if (event.currentTarget.value.length >= 1) {
+      let element = event.srcElement.nextElementSibling; // get the sibling element
+
+      if (element == null) {
+        // check if its null
+        return;
+      } else {
+        element.focus(); // focus if not null
+      }
+    }
+  }
 }
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -17,31 +17,4 @@ export class LandingComponent implements OnInit {
       this.router.navigate(['session', res.sessionId]);
     });
   }
-
-  joinSession(id: string) {
-    id = id.toUpperCase();
-    this.router.navigate(['session', id]);
-  }
-
-  inputKey(event) {
-    if (event.key === 'Backspace') {
-      let element = event.srcElement.previousElementSibling; // get the sibling element
-
-      if (element == null) {
-        // check if its null
-        return;
-      } else {
-        element.focus(); // focus if not null
-      }
-    } else if (event.currentTarget.value.length >= 1) {
-      let element = event.srcElement.nextElementSibling; // get the sibling element
-
-      if (element == null) {
-        // check if its null
-        return;
-      } else {
-        element.focus(); // focus if not null
-      }
-    }
-  }
 }
