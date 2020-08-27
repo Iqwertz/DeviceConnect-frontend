7e487e4f8668f4325a562c2e378016723c9ba032
Julius <juliushussl@gmail.com>
2020-08-27 13:41:42 +0200

    cant reach server error

--- src/app/components/create-session/create-session.component.ts
+++ src/app/components/create-session/create-session.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConnectService } from '../../services/connect.service';
 import { Router } from '@angular/router';
+import { UserAlertService } from '../../services/user-alert.service';
 
 @Component({
   selector: 'app-create-session',
@@ -8,13 +9,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-session.component.scss'],
 })
 export class CreateSessionComponent implements OnInit {
-  constructor(private connectService: ConnectService, private router: Router) {}
+  constructor(
+    private connectService: ConnectService,
+    private router: Router,
+    private uAlert: UserAlertService
+  ) {}
 
   ngOnInit(): void {}
 
   createSession() {
-    this.connectService.newSession().subscribe((res) => {
-      this.router.navigate(['session', res.sessionId]);
-    });
+    this.connectService.newSession().subscribe(
+      (res) => {
+        this.router.navigate(['session', res.sessionId]);
+      },
+      (err) => {
+        this.uAlert.setUserAlert(`Couldn't reach server`, 'error');
+      }
+    );
   }
 }
